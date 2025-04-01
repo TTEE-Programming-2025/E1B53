0c03b8dcc8a1ada263de1d8e2e6b714bb308b856
joie chou <smallbookjj@gmail.com>
2025-04-02 01:19:18 +0800

    write first HW

---
+++ P1E1B53.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main (void)
+{
+
+
+
+ 
+    printf("************************************\n");
+    printf("*                                  *\n");
+    printf("*  歡迎來到 E1B53 的作業區！         *\n");
+    printf("*                                  *\n");
+    printf("************************************\n");
+    printf("*                                  *\n");
+    printf("*        .     *     。            *\n");
+    printf("*           *       .             *\n");
+    printf("*        。          *             *\n");
+    printf("*           *       。            *\n");
+    printf("*        *      。    *           *\n");
+    printf("*                                  *\n");
+ 
+
+
+	
+	system("pause");
+	return 0;
+}
